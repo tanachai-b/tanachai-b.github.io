cd733057fe6f9dba8ad4725006856f631a8efbb1
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-06-02 15:34:26 +0700

    improve scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {
   Card,
+  CardBody,
   CardHeader,
   Container,
   DataHeader,
@@ -21,83 +22,85 @@ export default function App() {
           }
         />
 
-        <DataHeader title="GitHub Page" />
+        <CardBody>
+          <DataHeader title="GitHub Page" />
 
-        <DataRow
-          icon="home"
-          color="#808080"
-          title="tanachai-b.github.io"
-          path="tanachai-b.github.io"
-        />
+          <DataRow
+            icon="home"
+            color="#808080"
+            title="tanachai-b.github.io"
+            path="tanachai-b.github.io"
+          />
 
-        <DataHeader title="Vite.js - React - Typescript" />
+          <DataHeader title="Vite.js - React - Typescript" />
 
-        <DataRow
-          icon="sticky_note_2"
-          color="#ffd000"
-          title="Sticky Notes"
-          path="sticky-notes"
-        />
+          <DataRow
+            icon="sticky_note_2"
+            color="#ffd000"
+            title="Sticky Notes"
+            path="sticky-notes"
+          />
 
-        <DataHeader title="Next.js - React - Typescript" />
+          <DataHeader title="Next.js - React - Typescript" />
 
-        <DataRow
-          icon="account_balance"
-          color="#00c080"
-          title="Personal Finance"
-          path="personal-finance"
-        />
-        <DataRow
-          icon="watch"
-          color="#80ff00"
-          title="Watch Face"
-          path="watch-face"
-        />
-        <DataRow
-          icon="palette"
-          color="#ff6000"
-          title="Color Palette"
-          path="color-palette"
-        />
-        <DataRow
-          icon="keyboard"
-          color="#6000ff"
-          title="Thai Keyboard"
-          path="thai-keyboard"
-        />
-        <DataRow
-          icon="calendar_month"
-          color="#ff40e0"
-          title="Next.js"
-          path="calendar"
-        />
+          <DataRow
+            icon="account_balance"
+            color="#00c080"
+            title="Personal Finance"
+            path="personal-finance"
+          />
+          <DataRow
+            icon="watch"
+            color="#80ff00"
+            title="Watch Face"
+            path="watch-face"
+          />
+          <DataRow
+            icon="palette"
+            color="#ff6000"
+            title="Color Palette"
+            path="color-palette"
+          />
+          <DataRow
+            icon="keyboard"
+            color="#6000ff"
+            title="Thai Keyboard"
+            path="thai-keyboard"
+          />
+          <DataRow
+            icon="calendar_month"
+            color="#ff40e0"
+            title="Next.js"
+            path="calendar"
+          />
 
-        <DataHeader title="Plain Javscript" />
+          <DataHeader title="Plain Javscript" />
 
-        <DataRow
-          icon="deployed_code"
-          color="#00c0ff"
-          title="Equations 3D"
-          path="equations-3d"
-        />
-        <DataRow
-          icon="rocket_launch"
-          color="#0000ff"
-          title="Starship Command"
-          path="starship-command"
-        />
-        <DataRow
-          icon="public"
-          color="#6000ff"
-          title="Orbit Lines"
-          path="orbit-lines"
-        />
-        <DataRow
-          icon="directions_car"
-          color="#ff0000"
-          title="Car Simulator"
-          path="car-simulator"
-        />
+          <DataRow
+            icon="deployed_code"
+            color="#00c0ff"
+            title="Equations 3D"
+            path="equations-3d"
+          />
+          <DataRow
+            icon="rocket_launch"
+            color="#0000ff"
+            title="Starship Command"
+            path="starship-command"
+          />
+          <DataRow
+            icon="public"
+            color="#6000ff"
+            title="Orbit Lines"
+            path="orbit-lines"
+          />
+          <DataRow
+            icon="directions_car"
+            color="#ff0000"
+            title="Car Simulator"
+            path="car-simulator"
+          />
+        </CardBody>
       </Card>
     </Container>
   );

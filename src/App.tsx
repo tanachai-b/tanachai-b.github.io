d8d32d681f1583e3b3374568d0fda4c8a01dc967
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-06-02 13:28:56 +0700

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,96 @@ export default function App() {
 
         <DataHeader title="Vite.js - React - Typescript" />
 
-        <GitRow label="Sticky Notes" gitPath="sticky-notes" />
+        <GitRow
+          icon="sticky_note_2"
+          color="#ffd000"
+          label="Sticky Notes"
+          gitPath="sticky-notes"
+        />
 
         <DataHeader title="Next.js - React - Typescript" />
 
-        <GitRow label="Personal Finance" gitPath="personal-finance" />
-        <GitRow label="Watch Face" gitPath="watch-face" />
-        <GitRow label="Color Palette" gitPath="color-palette" />
-        <GitRow label="Thai Keyboard" gitPath="thai-keyboard" />
-        <GitRow label="Next.js" gitPath="calendar" />
+        <GitRow
+          icon="account_balance"
+          color="#00c080"
+          label="Personal Finance"
+          gitPath="personal-finance"
+        />
+        <GitRow
+          icon="watch"
+          color="#80ff00"
+          label="Watch Face"
+          gitPath="watch-face"
+        />
+        <GitRow
+          icon="palette"
+          color="#ff6000"
+          label="Color Palette"
+          gitPath="color-palette"
+        />
+        <GitRow
+          icon="keyboard"
+          color="#6000ff"
+          label="Thai Keyboard"
+          gitPath="thai-keyboard"
+        />
+        <GitRow
+          icon="calendar_month"
+          color="#ff40e0"
+          label="Next.js"
+          gitPath="calendar"
+        />
 
         <DataHeader title="Plain Javscript" />
 
-        <GitRow label="Equations 3D" gitPath="equations-3d" />
-        <GitRow label="Starship Command" gitPath="starship-command" />
-        <GitRow label="Orbit Lines" gitPath="orbit-lines" />
-        <GitRow label="Car Simulator" gitPath="car-simulator" />
+        <GitRow
+          icon="deployed_code"
+          color="#00c0ff"
+          label="Equations 3D"
+          gitPath="equations-3d"
+        />
+        <GitRow
+          icon="rocket_launch"
+          color="#0000ff"
+          label="Starship Command"
+          gitPath="starship-command"
+        />
+        <GitRow
+          icon="public"
+          color="#6000ff"
+          label="Orbit Lines"
+          gitPath="orbit-lines"
+        />
+        <GitRow
+          icon="directions_car"
+          color="#ff0000"
+          label="Car Simulator"
+          gitPath="car-simulator"
+        />
       </Card>
     </Container>
   );
 }
 
-function GitRow({ label, gitPath }: { label: string; gitPath: string }) {
+function GitRow({
+  icon,
+  color,
+  label,
+  gitPath,
+}: {
+  icon: string;
+  color: string;
+  label: string;
+  gitPath: string;
+}) {
   return (
     <DataRow
+      icon={icon}
+      color={color}
       title={label}
       subtitle={gitPath}
-      website={`https://tanachai-b.github.io/${gitPath}`}
       gitHub={`https://github.com/tanachai-b/${gitPath}`}
+      website={`https://tanachai-b.github.io/${gitPath}`}
     />
   );
 }
--- src/components.tsx
+++ src/components.tsx
@@ -12,7 +12,6 @@ export function Container({ children }: { children: ReactNode }) {
 
         "flex",
         "flex-col",
-
         "items-center",
         "justify-center",
 
@@ -78,24 +77,52 @@ export function DataHeader({ title }: { title: string }) {
 }
 
 export function DataRow({
+  icon,
+  color,
   title,
   subtitle,
-  website,
   gitHub,
+  website,
 }: {
+  icon: string;
+  color: string;
   title: string;
   subtitle: string;
-  website: string;
   gitHub: string;
+  website: string;
 }) {
   return (
     <div className={cx("flex", "flex-row")}>
+      <Icon icon={icon} color={color} />
+
       <TitleSubtitle title={title} subtitle={subtitle} />
 
-      <Link label="Website" href={website} />
-
       <Link label="GitHub" href={gitHub} />
+
+      <Link label="Website" href={website} />
     </div>
+  );
+}
+
+function Icon({ icon, color }: { icon: string; color: string }) {
+  return (
+    <>
+      <div
+        className={cx(
+          "flex",
+          "flex-row",
+          "items-center",
+          "justify-center",
+
+          "p-[5px]",
+
+          "text-[20px]",
+        )}
+        style={{ color }}
+      >
+        <IconSpan icon={icon} />
+      </div>
+    </>
   );
 }
 
@@ -111,7 +138,7 @@ function TitleSubtitle({
       <div>{title}</div>
 
       <div className={cx("text-[#606060]", "italic", "text-[13px]")}>
-        {subtitle}
+        /{subtitle}
       </div>
     </div>
   );
@@ -121,19 +148,47 @@ function Link({ label, href }: { label: string; href: string }) {
   return (
     <div
       className={cx(
-        "w-[70px]",
+        "w-[80px]",
 
         "flex",
+        "flex-row",
         "items-center",
         "justify-center",
 
+        "gap-[2px]",
+
         "text-[13px]",
         "text-[#606060]",
+        "hover:font-bold",
       )}
     >
-      <a className={cx("hover:font-bold")} href={href} target="_blank">
+      <a href={href} target="_blank">
         {label}
       </a>
+
+      <IconSpan icon="open_in_new" />
     </div>
+  );
+}
+
+function IconSpan({ icon }: { icon: string }) {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+      />
+
+      <span
+        className={cx("material-symbols-rounded")}
+        style={{
+          fontSize: "inherit",
+          fontWeight: "inherit",
+          fontVariationSettings: "'FILL' 1, 'GRAD' 0, 'opsz' 24",
+        }}
+      >
+        {icon}
+      </span>
+    </>
   );
 }
